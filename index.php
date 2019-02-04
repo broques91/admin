d8702a1f64f88539ee0d77a8e02a210ecd21ca7c
Bastien Roques <showtime@imac-de-bastien.home>
2019-02-04 20:21:18 +0100

    add project

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// Projet PHP : Un espace membre
+
+// INSCRIPTION
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet PHP : Espace membre</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Inscription</h1>
+    
+</body>
+</html>
