de8056c0932ced7a20b0a2b1c2ed726a3b82e0a8
Bastien Roques <showtime@imac-de-bastien.home>
2019-02-04 20:34:07 +0100

    add form

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 // INSCRIPTION
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +15,28 @@
 </head>
 <body>
     <h1>Inscription</h1>
-    
+    <p>Bienvenue sur mon site, inscrivez-vous pour plus d'informations</p>
+
+    <form method="post" action="index.php">
+        <table>
+            <tr>
+                <td>Pseudo</td>
+                <td><input type="text" name="pseudo"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td><input type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Mot de passe</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td>Confirmation du mot de passe</td>
+                <td><input type="password" name="password_confirm"></td>
+            </tr>
+        </table>
+        <button type="submit">Inscription</button>
+    </form> 
 </body>
 </html>
