3e065ad845bd4eae30c7beea1d9445837e33ec54
Bastien Roques <showtime@imac-de-bastien.home>
2019-02-04 20:51:43 +0100

    add style form

--- index.php
+++ index.php
@@ -15,25 +15,26 @@
 </head>
 <body>
     <h1>Inscription</h1>
-    <p>Bienvenue sur mon site, inscrivez-vous pour plus d'informations</p>
+    <p>Bienvenue sur mon site, inscrivez-vous pour plus d'informations.</p>
+    <p>Déjà inscrit ? <a href="Connexion.php">Connectez-vous</a>
 
     <form method="post" action="index.php">
         <table>
             <tr>
                 <td>Pseudo</td>
-                <td><input type="text" name="pseudo"></td>
+                <td><input type="text" name="pseudo" placeholder="Ex : Bastien"></td>
             </tr>
             <tr>
                 <td>Email</td>
-                <td><input type="email" name="email"></td>
+                <td><input type="email" name="email" placeholder="Ex : example@gmail.com"></td>
             </tr>
             <tr>
                 <td>Mot de passe</td>
-                <td><input type="password" name="password"></td>
+                <td><input type="password" name="password" placeholder="Ex : ***** "></td>
             </tr>
             <tr>
                 <td>Confirmation du mot de passe</td>
-                <td><input type="password" name="password_confirm"></td>
+                <td><input type="password" name="password_confirm" placeholder="Ex : *****"></td>
             </tr>
         </table>
         <button type="submit">Inscription</button>
